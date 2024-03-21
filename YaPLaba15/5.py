9a97aa8c16ce7653222204c73505460319ed90e5
Vladimir-Rumyantsev <113893528+Vladimir-Rumyantsev@users.noreply.github.com>
2024-03-21 11:44:55 +0500

    Update 5.py

--- YaPLaba15/5.py
+++ YaPLaba15/5.py
@@ -1,65 +1,52 @@
-# Ещё не готово
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
-# class Node:
-#     def __init__(self, data):
-#         # Инициализирует узел с данными, следующим и предыдущим узлами
-#         self.data = data
-#         self.next = None
-#         self.prev = None
+class IntStack:
+    def __init__(self):
+        self.head = None
 
-# class SortedList:
-#     def __init__(self):
-#         # Создаёт узел-связку, который будет служить началом и концом списка
-#         self.sentinel = Node(None)
-#         self.sentinel.next = self.sentinel
-#         self.sentinel.prev = self.sentinel
+    def insert_a_value(self, value):
+        new_node = Node(value)
+        if (self.head is None) or (self.head.data > value):
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next and (current.next.data <= value):
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
 
-#     def add(self, data):
-#         # Создаёт новый узел с данными, которые нужно добавить
-#         new_node = Node(data)
+    def display_queue(self):
+        current = self.head
+        line = ''
+        while current:
+            line = f'{line} {current.data}'
+            current = current.next
+        if self.head:
+            return line, self.head.data
+        return None, None
 
-#         # Находим правильную позицию для вставки нового узла
-#         current = self.sentinel
-#         while current.next != self.sentinel and current.next.data > data:
-#             current = current.next
 
-#         # Вставляет новый узел между текущим и следующим за ним узлами
-#         new_node.prev = current
-#         new_node.next = current.next
-#         current.next.prev = new_node
-#         current.next = new_node
+numbers = list(map(int, input(f'\nВведите через пробел набор чисел для списка: ').split()))
+P1 = IntStack()
+x = None
+if len(numbers) >= 1:
+    for i in range(len(numbers) - 1, -1, -1):
+        y = x
+        x = Node(numbers[i])
+        x.next = y
+P1.head = x
+P2 = P1
 
-#     def get_sorted_list(self):
-#         # Создаёт список для хранения отсортированных данных
-#         result = []
-
-#         # Перебирает узлы в нашем связном списке и добавляем данные в результирующий список
-#         current = self.sentinel.next
-#         while current != self.sentinel:
-#             result.append(current.data)
-#             current = current.next
-
-#         # Возвращает результирующий список в порядке возрастания
-#         return result[::1]
-
-# def read_input_file(file_path):
-#     with open(file_path, 'r') as file:
-#         n = int(file.readline().strip())
-#         numbers = [int(x) for x in file.readline().strip().split()]
-
-#     return n, numbers
-
-# def main():
-#     file_path = "input.txt"  # Заменяет путь к входному файлу
-#     n, numbers = read_input_file(file_path)
-
-#     sorted_list = SortedList()
-#     for num in numbers:
-#         sorted_list.add(num)
-
-#     result = sorted_list.get_sorted_list()
-#     print(result)
-
-# if name == "__main__":
-#     main()
+line, first = P2.display_queue()
+print(f'\nКопия списка сейчас:\n{line}')
+print(f'\nПервый элемент копии списка сейчас: {first}')
+m = int(input('\nВведите значение M: '))
+P2.insert_a_value(m)
+line, first = P2.display_queue()
+print(f'\nКопия списка после изменений:\n{line}')
+print(f'\nПервый элемент копии списка после изменений: {first}')
