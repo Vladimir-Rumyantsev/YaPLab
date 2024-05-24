600f72878641c10d2d3c3f988ef4c37dba3c631c
Vladimir-Rumyantsev <113893528+Vladimir-Rumyantsev@users.noreply.github.com>
2024-05-24 21:42:36 +0500

    Update 2.py

--- YaPLaba22/2.py
+++ YaPLaba22/2.py
@@ -8,10 +8,17 @@ class Node:
 def inoder(root):
     if root is not None:
         print("(", end="")
-        print(root.val, end="")
         inoder(root.left)
+        print(root.val, end="")
         inoder(root.right)
         print(")", end="")
+
+
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.val, end=" ")
+        inorder(root.right)
 
 
 def new_deleteNode(root, key):
@@ -23,7 +30,44 @@ def new_deleteNode(root, key):
     elif key > root.val:
         root.right = new_deleteNode(root.right, key)
     else:
-        root = root.right
+        if root.right is not None:
+            root.val = root.right.val
+            root.right = root.right.right
+        else:
+            root = root.left
+
+    return root
+
+
+def minValueNode(node):
+    current = node
+    while current and current.left is not None:
+        current = current.left
+    return current
+
+
+def deleteNode(root, key):
+    if root is None:
+        return root
+
+    if key < root.val:
+        root.left = deleteNode(root.left, key)
+    elif key > root.val:
+        root.right = deleteNode(root.right, key)
+    else:
+        # узел концевой или с одним дочерним
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        # узел с двумя дочерними:
+        temp = minValueNode(root.right)
+        root.val = temp.val
+        root.right = deleteNode(root.right, temp.val)
 
     return root
 
@@ -37,6 +81,14 @@ if __name__ == '__main__':
     #  1   3   6    9
     #       \   \
     #        4   7
+
+    #        6
+    #     /     \
+    #    2       8
+    #   / \     /  \
+    #  1   3   7    9
+    #       \   \
+    #        4
 
     root = Node(5)
     root.left = Node(2)
@@ -52,7 +104,7 @@ if __name__ == '__main__':
     print('\nСостояние дерева сейчас: ', end='')
     inoder(root)
 
-    root = new_deleteNode(root, int(input('\n\nВведите значение удаляемой вершины: ')))
+    root = deleteNode(root, int(input('\n\nВведите значение удаляемой вершины: ')))
 
     print('\nДерево после удаления: ', end='')
     inoder(root)
